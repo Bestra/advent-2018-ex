4c4cd26bc163e239aad078c7b0ec873e319fdde5
Chris Westra <cwestra@gmail.com>
2019-01-08 07:38:58 -0500

    Start day 6

--- lib/advent.ex
+++ lib/advent.ex
@@ -17,9 +17,11 @@ defmodule Advent do
   end
 
   def read_file(id) do
-    {:ok, s} = Path.expand("./inputs/#{id}.txt")
-    |> IO.inspect(label: "file path")
-    |> File.read()
+    {:ok, s} =
+      Path.expand("./inputs/#{id}.txt")
+      |> IO.inspect(label: "file path")
+      |> File.read()
+
     String.split(s, "\n")
   end
 
@@ -34,12 +36,12 @@ defmodule Advent do
       case acc do
         [] ->
           [char]
+
         [h | t] when h + 32 === char or h - 32 === char ->
           t
+
         [_h | _] ->
           [char | acc]
-        n when is_integer(n) ->
-          [n | acc]
       end
     end
 
@@ -58,10 +60,65 @@ defmodule Advent do
 
     def part2() do
       s = Advent.read_file(5) |> List.first() |> to_charlist()
+
       Enum.map(?a..?z, fn x ->
         remove_pair(s, x) |> react() |> length()
       end)
       |> Enum.sort()
+    end
+  end
+
+  defmodule Day6 do
+    defstruct inputs: [], bounding_box: nil, grid: []
+    def sample_input() do
+      ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
+    end
+
+    def parse_line(l) do
+      [x, y] = String.split(l, ", ")
+
+      {a, _} = Integer.parse(x)
+      {b, _} = Integer.parse(y)
+      {a, b}
+    end
+
+    def inputs(s), do: Enum.map(s, &parse_line/1)
+
+    def bounding_box(i) do
+      xs = Enum.map(i, &elem(&1, 0))
+      {min_x, max_x} = xs |> Enum.min_max()
+
+      ys = Enum.map(i, &elem(&1, 1))
+      {min_y, max_y} = ys |> Enum.min_max()
+
+      {{min_x - 1, min_y - 1}, {max_x + 1, max_y + 1}}
+    end
+
+    def grid({{x1, y1}, {x2, y2}}) do
+      for x <- x1..x2, y <- y1..y2 do
+        p = case {x, y} do
+          _ when x in [x1, x2] or y in [y1, y2] ->
+            :exterior
+          _ ->
+            :interior
+        end
+        {{x, y}, p}
+      end
+    end
+
+    def make(input_list) do
+      i = inputs(input_list) |> Enum.with_index()
+      b = bounding_box(i)
+      g = grid(b)
+      %Day6{inputs: i, bounding_box: b, grid: g}
+    end
+
+    def nearest({px, py}, inputs) do
+      Enum.map(inputs, fn {{x, y}, i} ->
+        {abs(x - px) + abs(y - py), i}
+      end)
+      |> Enum.sort_by(fn {d, _} -> d end)
+      |> List.first()
     end
   end
 end
--- test/advent_test.exs
+++ test/advent_test.exs
@@ -8,12 +8,36 @@ defmodule AdventTest do
 
   describe "Day5" do
     alias Advent.Day5
+
     test "opposites destroy" do
-      assert Day5.react("aA") == ""
-      assert Day5.react("abBA") == ""
-      assert Day5.react("abAB") == "abAB"
-      assert Day5.react("aabAAB") == "aabAAB"
-      assert Day5.react("dabAcCaCBAcCcaDA") == "dabCBAcaDA"
+      assert Day5.react('aA') == ''
+      assert Day5.react('abBA') == ''
+      assert Day5.react('abAB') == 'abAB'
+      assert Day5.react('aabAAB') == 'aabAAB'
+      assert Day5.react('dabAcCaCBAcCcaDA') == 'dabCBAcaDA'
+    end
+  end
+
+  describe "Day6" do
+    alias Advent.Day6
+    input = ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
+
+    test "parsing" do
+      assert Day6.parse_line("12, 1") == {12, 1}
+    end
+
+    test "bounding_box" do
+      input = ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
+
+      assert Day6.inputs(input) |> Day6.bounding_box() == {{0, 0}, {9, 10}}
+    end
+
+    test "nearest" do
+      i = ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
+      |> Day6.inputs()
+      |> Enum.with_index()
+
+      assert Day6.nearest({2, 2}, i) == {2, 0}
     end
   end
 end
