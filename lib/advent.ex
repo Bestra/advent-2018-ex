0c834c558079ce32fd85df1e8ab4f2d1c4eac9a8
Chris Westra <cwestra@gmail.com>
2019-01-11 13:15:24 -0500

    Add dialyzer, use structs

--- lib/advent.ex
+++ lib/advent.ex
@@ -74,24 +74,33 @@ defmodule Advent do
       ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
     end
 
+    defmodule Input do
+      defstruct x: 0, y: 0
+    end
+
     def parse_line(l) do
       [x, y] = String.split(l, ", ")
 
       {a, _} = Integer.parse(x)
       {b, _} = Integer.parse(y)
-      {a, b}
+      %Input{x: a, y: b}
     end
 
-    def inputs(s), do: Enum.map(s, &parse_line/1)
+    @spec parse_string_input(list(binary)) :: [Input.t]
+    def parse_string_input(s), do: Enum.map(s, &parse_line/1)
 
-    def bounding_box(i) do
-      xs = Enum.map(i, &elem(&1, 0))
+    def bounding_box(inputs) do
+      xs = Enum.map(inputs, &(Map.get(&1, :x)))
       {min_x, max_x} = xs |> Enum.min_max()
 
-      ys = Enum.map(i, &elem(&1, 1))
+      ys = Enum.map(inputs, &(Map.get(&1, :y)))
       {min_y, max_y} = ys |> Enum.min_max()
 
       {{min_x - 1, min_y - 1}, {max_x + 1, max_y + 1}}
+    end
+
+    defmodule GridCell do
+      defstruct x: 0, y: 0, p: :interior, nearest: :unset
     end
 
     def grid({{x1, y1}, {x2, y2}}) do
@@ -102,23 +111,43 @@ defmodule Advent do
           _ ->
             :interior
         end
-        {{x, y}, p}
+        %GridCell{x: x, y: y, p: p}
       end
     end
 
     def make(input_list) do
-      i = inputs(input_list) |> Enum.with_index()
+      i = parse_string_input(input_list)
       b = bounding_box(i)
       g = grid(b)
       %Day6{inputs: i, bounding_box: b, grid: g}
     end
 
-    def nearest({px, py}, inputs) do
-      Enum.map(inputs, fn {{x, y}, i} ->
-        {abs(x - px) + abs(y - py), i}
+    def nearest(%GridCell{x: px, y: py}, inputs) do
+      Enum.map(inputs, fn {%Input{x: x, y: y}, i} ->
+        %{distance: abs(x - px) + abs(y - py), idx: i}
       end)
-      |> Enum.sort_by(fn {d, _} -> d end)
+      |> Enum.sort_by(fn %{distance: d} -> d end)
       |> List.first()
+    end
+
+    def set_nearest(%Day6{grid: g, inputs: i} = d) do
+      indexed = Enum.with_index(i)
+      updated_grid = for p <- g do
+        %{idx: idx} = nearest(p, indexed)
+        %{p | nearest: idx}
+      end
+
+      %{d | grid: updated_grid}
+    end
+
+    def nearest_groups(%Day6{grid: g}) do
+      g |> Enum.group_by(fn %GridCell{nearest: n} -> n end)
+    end
+
+    def interior_groups(group_map) do
+      :maps.filter(fn _, v ->
+        Enum.all?(v, fn %GridCell{p: p} -> p == :interior end)
+      end, group_map)
     end
   end
 end
--- test/advent_test.exs
+++ test/advent_test.exs
@@ -20,24 +20,35 @@ defmodule AdventTest do
 
   describe "Day6" do
     alias Advent.Day6
-    input = ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
+    setup do
+      {:ok, input: ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]}
+    end
 
     test "parsing" do
-      assert Day6.parse_line("12, 1") == {12, 1}
+      assert Day6.parse_line("12, 1") == %Day6.Input{x: 12, y: 1}
     end
 
-    test "bounding_box" do
-      input = ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
-
-      assert Day6.inputs(input) |> Day6.bounding_box() == {{0, 0}, {9, 10}}
+    test "bounding_box", %{input: input} do
+      assert Day6.parse_string_input(input) |> Day6.bounding_box() == {{0, 0}, {9, 10}}
     end
 
-    test "nearest" do
-      i = ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
-      |> Day6.inputs()
+    test "nearest", %{input: i} do
+      inputs = i
+      |> Day6.parse_string_input()
       |> Enum.with_index()
 
-      assert Day6.nearest({2, 2}, i) == {2, 0}
+      assert Day6.nearest(%Day6.GridCell{x: 2, y: 2, p: :interior}, inputs) == %{distance: 2, idx: 0}
+    end
+
+    test "nearest groups", %{input: i} do
+      res = i
+      |> Day6.make()
+      |> Day6.set_nearest()
+      |> Day6.nearest_groups()
+      |> Day6.interior_groups()
+      |> Map.keys()
+
+      assert res == [3, 4]
     end
   end
 end
